4d1e744a27aeb1360289986665cc009f8a99d126
Túlio Carvalho <tulio.cefetmg@gmail.com>
2025-02-16 00:17:35 -0300

    v0.0.0

--- main/main.c
+++ main/main.c
@@ -6,12 +6,6 @@
 
 void app_main(void)
 {
-    //ADC ADC1_0, ADC_3, ADC_6
-    //gpio_4 = G-SELECT = 1
-    //GPIO1_16 = SELF TEST = 0
-    //GPIO_17 = 0G_DETECT = INPUT
-    //GPPOI5 = SLEEP = 0
-
     mma7361_handle_t mma7361_handle;
     mma7361_config_t mma7361_config = {
         .x_channel = ADC_CHANNEL_0,
@@ -30,34 +24,11 @@ void app_main(void)
 
     int axis_x, axis_y, axis_z;
 
-    int cont = 0;
-
     while(1){
 
-        if(cont < 20){
             mma7361_read_3axes(&axis_x, &axis_y, &axis_z, mma7361_handle);
-        }
-        
         vTaskDelay(3000/portTICK_PERIOD_MS);
 
-        cont++;
-        
-        if(cont == 20){
-            mma7361_del(mma7361_handle);
-        }
-        
-        if (cont == 5){
-            mma7361_gselect(0,mma7361_handle);
-
-        }
-        if (cont == 10){
-            mma7361_selftest(1,mma7361_handle);
-            
-        }
-        if (cont == 15){
-            mma7361_sleep(0,mma7361_handle);
-            
-        }
     }
     
 
